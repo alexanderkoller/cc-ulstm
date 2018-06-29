282b771dd5b40fa8c06edde1e6f50372fd25e7fa
Alexander Koller <akoller@fb.com>
2018-06-29 16:52:59 +0200

    complete model that runs through (but doesnt seem to learn very quickly)

--- main.py
+++ main.py
@@ -1,28 +1,295 @@
+import json
+import sys
+from collections import defaultdict
+from queue import Queue
 
+import nltk
+import spacy
 import torch
+import torchtext
+from torch.optim import Adam
+from tqdm import tqdm
 
-from model import SequentialChart
+from model import SequentialChart, SnliModel
 
 bs = 3
 seqlen = 6
 hd = 2
 
-chart = torch.zeros(bs, seqlen, 2*hd)
-chart[:,0,:] = torch.rand(bs, 2*hd)
-chart[:,1,:] = torch.rand(bs, 2*hd)
-chart[:,2,:] = torch.rand(bs, 2*hd)
-# print(chart)
-
-model = SequentialChart(hd)
-
-for param in model.parameters():
-    print(type(param.data), param.size())
 
 
-operations = [[[(0,1)], [(0,1)], [(0,1)]], [[(1,2)], [(1,2)], [(1,2)]], [[(0,4),(3,2)], [(0,4),(0,0)], [(0,4),(0,0)]]] # TODO - dealing with diff ambiguity?
+glove = torchtext.vocab.GloVe(name='6B', dim=100)
 
-result = model(chart, operations, 3)
 
-print(result)
-# print(result.size())
+class Lexicon:
+    def __init__(self):
+        self.forward = {}
+        self.backward = []
 
+    def add(self, value):
+        if value in self.forward:
+            return self.forward[value]
+        else:
+            id = len(self.backward)
+            self.forward[value] = id
+            self.backward.append(value)
+            return id
+
+    # return None if does not exist
+    def get_id(self, value):
+        return self.forward.get(value)
+
+    # return None if does not exist
+    def get_value(self, id):
+        if id >= len(self.backward):
+            return None
+        else:
+            return self.backward[id]
+
+    # add null entries until the next ID is next_id
+    def pad(self, next_id):
+        while len(self.backward) < next_id:
+            self.add(f"***dummy{len(self.backward)}***")
+
+    def __str__(self):
+        return str(self.forward)
+
+    def __repr__(self):
+        return str(self)
+
+def allowed(sentence_id, start, end):
+    return True
+
+def parse(sentence, sentence_index, maxlen, is_edge_allowed):
+    n = len(sentence)
+    operations = []
+    edge_lex = Lexicon()
+
+    # lexical productions
+    for i in range(len(sentence)):
+        item = (i,i+1)
+        id = edge_lex.add(item)
+
+    edge_lex.pad(maxlen)
+
+
+    # build larger constituents, CKY-style
+    for width in range(2, n+1):  # 2 <= width <= n
+        for start in range(n-width+1):   # 0 <= start <= n-width
+            # print(f"\nItem: {start}-{start+width}")
+            if is_edge_allowed(sentence_index, start, start+width):
+                ops = [] # decompositions for this item
+
+                for split in range(1,width):  # 1 <= split <= width-1, width of left part
+                    # print(f"Consider {start}-{start+split}-{start+width}")
+                    left_item = (start, start+split)
+                    right_item = (start+split, start+width)
+                    left_id = edge_lex.get_id(left_item)
+                    right_id = edge_lex.get_id(right_item)
+
+                    if left_id is not None and right_id is not None:
+                        # split exists
+                        ops.append((left_id,right_id))
+                        # print(f"Add op: #{left_id} to #{right_id}")
+
+                if len(ops) > 0:
+                    item = (start, start + width)
+                    id = edge_lex.add(item)
+                    operations.append(ops)
+                    # print(f"Added ops for item {item} #{id}")
+
+    return operations, edge_lex
+
+
+def pad_list(list_to_pad, desired_length, filler):
+    while len(list_to_pad) < desired_length:
+        if type(filler) is list:
+            x = filler.copy()
+        else:
+            x = filler
+
+        list_to_pad.append(x)
+
+def pad_operations(all_operations, max_ops_length):
+    num_sentences = len(all_operations)
+
+    # pad the ops sequence for each sentence to the max length of ops sequences
+    for ops in all_operations:
+        pad_list(ops, max_ops_length, [(0,0)])
+
+    # pad the decompositions at each position to the same length
+    for pos in range(max_ops_length):
+        lengths = [len(all_operations[i][pos]) for i in range(num_sentences)]
+        maxlen = max(lengths)
+
+        for i in range(num_sentences):
+            pad_list(all_operations[i][pos], maxlen, (0,0))
+
+
+def convert_sentences(sentences, is_edge_allowed):
+    # sentences: list(list(int)); len(sentences) = bs; len(sentences[0]) = length of first sentence; sentences[i][j] = id of j-th word in i-th sentence
+    # is_edge_allowed: function, is_edge_allowed(i,j,k) = True iff edge from j-k in i is allowed
+    # model: a SequentialChart object, which is used to map word ids to word embeddings
+
+    max_sentence_length = max([len(sent) for sent in sentences])
+    bs = len(sentences)
+    parses = [parse(sent, sent_index, max_sentence_length, is_edge_allowed) for sent_index, sent in enumerate(sentences)]
+    all_operations, all_edgelex = zip(*parses)
+
+    num_decomps = [[len(decomps) for decomps in sent_operations] for sent_operations in all_operations]
+    original_lengths = [len(sent_operations) for sent_operations in all_operations]
+    max_ops_length = max(original_lengths)
+    pad_operations(all_operations, max_ops_length)
+
+    return all_operations, original_lengths, all_edgelex
+
+import mmap
+
+def get_num_lines(file_path):
+    fp = open(file_path, "r+")
+    buf = mmap.mmap(fp.fileno(), 0)
+    lines = 0
+    while buf.readline():
+        lines += 1
+    return lines
+
+
+# TODO - I should bucket sentences so sentences of similar length are grouped together.
+# TODO - Then only pad sentences in same bucket to same ops length.
+# For now, the code is not wrong, will just be inefficient.
+
+def tokenize(sentence):
+    # NLTK - much much faster than Spacy
+    return [token.lower() for token in nltk.word_tokenize(sentence)]
+
+    # Spacy
+    # tokens = tokenizer(sentence)
+    # return [token.text.lower() for token in tokens]
+
+
+
+
+
+# word_lex = Lexicon()
+
+tokenizer = spacy.load('en_core_web_sm')
+
+def word_lookup(words):
+    def lookup(word):
+        if word in glove.stoi:
+            return glove.stoi[word]
+        else:
+            return -1
+
+    return [lookup(word) for word in words]
+
+# sentences = []
+MAX_SENTENCES = 100
+BATCHSIZE = 10
+
+training_sent1 = []
+training_sent2 = []
+training_labels = []
+
+max_sentence1_length = 0
+max_sentence2_length = 0
+
+snli_label_dict = {"neutral": 0, "contradiction":1, "entailment":2}
+
+train_file = "/Users/akoller/Downloads/snli_1.0/snli_1.0_train.jsonl"
+with open(train_file) as f:
+    for line in tqdm(f, desc="Reading training sentences", total=get_num_lines(train_file)):
+        j = json.loads(line)
+        sentence1 = word_lookup(tokenize(j["sentence1"]))
+        sentence2 = word_lookup(tokenize(j["sentence2"]))
+        label = snli_label_dict[j["gold_label"]]
+
+        training_sent1.append(sentence1)
+        training_sent2.append(sentence2)
+        training_labels.append(label)
+
+        max_sentence1_length = max(max_sentence1_length, len(sentence1))
+        max_sentence2_length = max(max_sentence2_length, len(sentence2))
+
+        if len(training_labels) >= MAX_SENTENCES:
+            break
+
+
+
+# set up model and optimizer
+model = SnliModel(10, 100, 10, glove)
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = Adam(model.parameters())
+
+
+
+
+
+#
+# sentences1 = [x[0] for x in training_data]
+# sentences2 = [x[1] for x in training_data]
+#
+# print("convert")
+# ops1, oopl1, edgelex1 = convert_sentences(sentences1, allowed)
+# ops2, oopl2, edgelex2 = convert_sentences(sentences2, allowed)
+# print("forward")
+# print(model(sentences1, ops1, oopl1, sentences2, ops2, oopl2))
+# sys.exit(0)
+
+
+
+for epoch in range(10):
+    optimizer.zero_grad()
+    total_loss = 0
+
+    for batch in range(int(MAX_SENTENCES/BATCHSIZE)):
+        print(f"\nbatch {batch}")
+        s1 = training_sent1[batch*BATCHSIZE : (batch+1)*BATCHSIZE]
+        s2 = training_sent2[batch*BATCHSIZE : (batch+1)*BATCHSIZE]
+        lab = torch.LongTensor(training_labels[batch*BATCHSIZE : (batch+1)*BATCHSIZE])
+
+        print("convert")
+        # TODO - I think this can happen once in the beginning
+        ops1, oopl1, edgelex1 = convert_sentences(s1, allowed)
+        ops2, oopl2, edgelex2 = convert_sentences(s2, allowed)
+        print(f"max oopl1: {max(oopl1)}")
+        print(f"max oopl2: {max(oopl2)}")
+
+        print("forward")
+        predictions = model(s1, ops1, oopl1, s2, ops2, oopl2)
+
+        loss = criterion(predictions, lab)
+        total_loss += loss.item()
+
+        loss.backward()
+        optimizer.step()
+
+        print(f"loss in batch {batch}: {loss.item()}")
+
+    print(total_loss)
+
+
+
+
+
+
+#
+#
+# all_operations, original_opseq_lengths, all_edgelex = convert_sentences(sentences, allowed, model)
+# max_sentence_length = max([len(sent) for sent in sentences])
+#
+# for i, sentence in enumerate(sentences):
+#     print(f"{i+1}: {sentence}")
+#
+# # print(f"msl: {max_sentence_length}; ool: {original_opseq_lengths}")
+#
+# chart = model.chart_for_batch(sentences, glove, original_opseq_lengths, max_sentence_length)
+#
+# result = model(chart, all_operations, max_sentence_length, original_opseq_lengths)
+# print(result)
+# #def forward(self, chart, operations, start_index, original_operations_lengths):
+#
+#
+# print(chart.size())
+# print(chart[0,0,:])
+# sys.exit(0)
--- model.py
+++ model.py
@@ -1,44 +1,107 @@
 import torch
 import torch.nn.functional as F
-from torch.nn import Module, Parameter
+from torch.nn import Module, Parameter, Linear
 
+
+class SnliModel(Module):
+    def __init__(self, hidden_dim, embedding_dim, mlp_dim, glove):
+        super(SnliModel, self).__init__()
+
+        self.sentence_model = SequentialChart(hidden_dim, embedding_dim, glove)
+        self.clayer1 = Linear(2*hidden_dim, mlp_dim)
+        self.clayer2 = Linear(mlp_dim, 3)
+
+    def forward(self, sentences1, operations1, oopl1, sentences2, operations2, oopl2):
+        s1 = self.sentence_model(sentences1, operations1, oopl1) # (bs, hd)
+        s2 = self.sentence_model(sentences2, operations2, oopl2) # (bs, hd)
+        conc = torch.cat((s1, s2), dim=1) # (bs, 2*hd)
+
+        internal1 = F.relu(self.clayer1(conc)) # (bs, mlp_dim)
+        internal2 = F.softmax(self.clayer2(internal1), dim=1) # (bs, 3)
+
+        return internal2
 
 class SequentialChart(Module):
-    def __init__(self, hidden_dim):
+    def __init__(self, hidden_dim, embedding_dim, glove):
         super(SequentialChart, self).__init__()
 
         self.hidden_dim = hidden_dim
+        self.embedding_dim = embedding_dim
+        self.glove = glove
 
+        self.W = Parameter(torch.zeros((5 * self.hidden_dim, self.embedding_dim), requires_grad=True))
         self.U = Parameter(torch.zeros((5 * self.hidden_dim, 2 * self.hidden_dim), requires_grad=True))
         self.b = Parameter(torch.zeros((5 * self.hidden_dim, ), requires_grad=True))
         self.energy_u = Parameter(0.01*torch.ones((self.hidden_dim,), requires_grad=True))
 
-        for p in (self.U, self.energy_u):
+        self.unk_embedding = Parameter(torch.zeros((self.embedding_dim,), requires_grad=True))
+
+        for p in (self.W, self.U, self.energy_u, self.unk_embedding):
             torch.nn.init.uniform_(p)
 
         self.temperature = torch.tensor([1.])
 
-    def forward(self, chart, operations, start_index):
-        # chart: (bs, seqlen, 2*hd)           chart[i,j,:] = (c,h) for position #j of sentence #i in batch
-        # with chart[0,:,:] all zero; so zero premises can be used for nop
+    def ch(self, preact, ccL, ccR):
+        i = F.sigmoid(preact[:, :, :self.hidden_dim])  # (bs, amb, hd)
+        fL = F.sigmoid(preact[:, :, self.hidden_dim:2 * self.hidden_dim])  # (bs,amb,hd)
+        fR = F.sigmoid(preact[:, :, 2 * self.hidden_dim:3 * self.hidden_dim])  # (bs,amb,hd)
+        o = F.sigmoid(preact[:, :, 3 * self.hidden_dim:4 * self.hidden_dim])  # (bs,amb,hd)
+        u = F.tanh(preact[:, :, 4 * self.hidden_dim:5 * self.hidden_dim])  # (bs,amb,hd)
 
-        # operations: [[[(L1,R1), (L1',R1') ...],...,[(Lb,Rb), (Lb',Rb') ...]], ..., ]    # b = batchsize; Li, Ri are indices into chart dimension 1
+        c = fL * ccL + fR * ccR + i * u  # (bs,amb,hd)
+        h = o * F.tanh(c)  # (bs,amb,hd)
+        return c,h
+
+    def chart_for_batch(self, sentences, word_embeddings, original_opseq_lengths, max_sentence_length):
+        bs = len(sentences)
+        chart = torch.zeros(bs, max_sentence_length + max(original_opseq_lengths), 2 * self.hidden_dim)
+
+        for i in range(max_sentence_length):
+            preact = torch.zeros(bs, 1, 5 * self.hidden_dim)
+            for b, sentence in enumerate(sentences):
+                if i >= len(sentence):
+                    # no word at this position
+                    preact[b] = torch.zeros((1,5*self.hidden_dim))
+                else:
+                    wordid = sentence[i]
+                    emb = word_embeddings.vectors[wordid] if wordid >= 0 else self.unk_embedding  # (embdim)
+                    preact[b] = self.W @ emb + self.b  # (5*hd), broadcast into (1, 5*hd)
+
+            c,h = self.ch(preact, 0, 0) # 2x (bs, 1, hd)
+            chart[:, i, :self.hidden_dim] = c.squeeze(dim=1)
+            chart[:, i, self.hidden_dim:] = h.squeeze(dim=1)
+
+        return chart
+
+
+    def forward(self, sentences, operations, original_operations_lengths):
+        # chart: (bs, seqlen, 2*hd)           chart[i,j,:] = (c,h) for position #j of sentence #i in batch
+        # with chart[0,:,:] all zero; so zero premises can be used for nop  ## XXX this is no longer true
+
+        # operations = list(opseq), where len(operations) = #sentences in batch, and opseq is list of operations for this sentence
+        # opseq = list(op), where len(opseq) = #parsing steps for this sentence
+        # ops = [(L,R), ..., (L,R)], i.e. all ways in which this item can be built from a left and right part; L,R are indices into chart rows (dimension 1)
+
+        # operations and opseq have been padded to max length. Original lengths of the opseqs (before padding) are in original_operations_lengths.
+
+        max_sentence_length = max(len(sentence) for sentence in sentences)
+        chart = self.chart_for_batch(sentences, self.glove, original_operations_lengths, max_sentence_length)
+        start_index = max_sentence_length
 
         bs = chart.size()[0]
+        assert bs == len(operations)
 
-        for step, ops in enumerate(operations):
-            # ops : [[(L1,R1), (L1',R1') ...], ..., [(Lb,Rb), (Lb',Rb') ...]]
-            # ops[b] is a list of decompositions (L,R) for the chart entry #step+start_index for sentence #b in batch
+        max_opseq_length = max([len(opseq) for opseq in operations])  # max length of the opseq's
 
-            # TODO - this is probably expensive. See if they can be made faster.
+        for step in range(max_opseq_length):
+            # TODO - this is probably expensive. See if it can be made faster.
             chart_entries = [
                 torch.stack(
-                    [torch.stack((chart[b,l,:], chart[b,r,:])) for l,r in decomps] # list of amb entries of shape (2, 2*hd)
+                    [torch.stack((chart[b,l,:], chart[b,r,:])) for l,r in operations[b][step]] # list of amb entries of shape (2, 2*hd)
                 ) # (amb, 2, 2*hd)
-                for b, decomps in enumerate(ops)]   # len=bs; chart_entries[b]: (2, 2*hd)
-            chart_entries = torch.stack(chart_entries) # (bs, amb, 2, 2*hd)
+                for b in range(bs)]   # len=bs; chart_entries[b]: (2, 2*hd)
 
-            # print(f"ce: {chart_entries.size()}")
+            chart_entries = torch.stack(chart_entries) # (bs, amb, 2, 2*hd)
             amb = chart_entries.size()[1] # #decompositions of items in this step
 
             cc = chart_entries[:,:,:,:self.hidden_dim]   # (bs, amb, 2, hd)
@@ -46,39 +109,17 @@ class SequentialChart(Module):
             ccR = cc[:,:,1,:].squeeze(dim=2) # (bs, amb, hd)
             hh = chart_entries[:,:,:,self.hidden_dim:].contiguous().view(-1,2*self.hidden_dim,1)   # (bs*amb, 2*hd, 1)  # TODO - contiguous expensive?
 
-            # print(f"ccL: {ccL.size()}")
-            # print(f"U: {self.U.size()}")
-            # print(f"hh: {hh.size()}")
-            # print(f"b: {self.b.size()}")
-
-            UE = self.U.expand(bs*amb, -1, -1)  # (bs*amb, 5*hd, 2*hd)
-            # print(f"UE: {UE.size()}")
-
-
             # batched evaluation of TreeLSTM cell
+            UE = self.U.expand(bs*amb, -1, -1)  # (bs*amb, 5*hd, 2*hd)
             mult = torch.bmm(UE, hh)        # (bs*amb, 5*hd, 1)
             mult = mult.view(bs, amb, 5*self.hidden_dim) # (bs, amb, 5*hd)
-            # print(f"mult: {mult.size()}")
 
             preact = mult + self.b    # (bs, amb, 5*hd)
-            # print(f"preact: {preact.size()}")
-
-            i = F.sigmoid(preact[:, :, :self.hidden_dim]) # (bs, amb, hd)
-            fL = F.sigmoid(preact[:, :, self.hidden_dim:2*self.hidden_dim]) # (bs,amb,hd)
-            fR = F.sigmoid(preact[:, :, 2*self.hidden_dim:3*self.hidden_dim]) # (bs,amb,hd)
-            o = F.sigmoid(preact[:, :, 3*self.hidden_dim:4*self.hidden_dim]) # (bs,amb,hd)
-            u = F.tanh(preact[:, :, 4*self.hidden_dim:5*self.hidden_dim]) # (bs,amb,hd)
-
-            c = fL*ccL + fR*ccR + i*u   # (bs,amb,hd)
-            h = o * F.tanh(c)  # (bs,amb,hd)
-
-            # print(f"c: {c.size()}")
-            # print(f"h: {h.size()}")
+            c, h = self.ch(preact, ccL, ccR)
 
             # combine decompositions of this item
             expanded_u = self.energy_u.expand((bs, amb, -1)) # (bs, amb, hd)
             e = F.cosine_similarity(expanded_u, h, dim=2)    # (bs, amb)
-            # print(f"e: {e.size()}")
 
             s = F.softmax(e/self.temperature[0], dim=1).view(bs,amb,1)      # (bs, amb, 1)
             combined_c = (s*c).sum(dim=1) # (bs, hd)
@@ -88,6 +129,10 @@ class SequentialChart(Module):
             chart[:, start_index+step, :self.hidden_dim] = combined_c
             chart[:, start_index+step, self.hidden_dim:] = combined_h
 
-        return chart
+
+        # return a tensor with h for the final state of each sentence in the minibatch
+        ret = torch.stack([chart[b, original_operations_lengths[b]-1, self.hidden_dim:] for b in range(bs)]) # (bs, hd)
+        return ret
+
 
 
